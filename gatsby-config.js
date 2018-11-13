d7232d4652bd5e8e0ecba80cb8db7947d52848de
Jacine Luisi <jacine.luisi@gmail.com>
2018-11-13 15:17:07 -0500

    Markdown: Step 4.  Default config for `gatsby-transformer-remark`.

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,7 @@ module.exports = {
         name: "markdown-pages",
       },
     },
+    `gatsby-transformer-remark`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
